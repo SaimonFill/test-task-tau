28c129e1dba1cb3d55750d7a44fa4b9b04a265d5
SaimonFill <saimongabriel@hotmail.com>
2022-11-21 18:40:23 -0300

    feat: init project.

---
+++ src/main/java/com/test/taskTau/TaskTauApplication.java
@@ -0,0 +1,13 @@
+package com.test.taskTau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/taskTau/TaskTauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.taskTau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

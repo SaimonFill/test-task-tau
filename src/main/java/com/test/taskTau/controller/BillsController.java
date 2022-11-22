8645e24e59f8662681221be92a506fadcc44faf5
SaimonFill <saimongabriel@hotmail.com>
2022-11-22 11:53:13 -0300

    fix: adjust the BillsController parameter.

--- src/main/java/com/test/taskTau/controller/BillsController.java
+++ src/main/java/com/test/taskTau/controller/BillsController.java
@@ -2,10 +2,7 @@ package com.test.taskTau.controller;
 
 import com.test.taskTau.service.BillsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -16,8 +13,8 @@ public class BillsController {
 
     private final BillsService service;
 
-    @GetMapping(value = "/{bills}")
-    public int getMinimumCoins(@PathVariable BigDecimal bills) throws Exception {
-        return service.findMiniumCoins(bills);
+    @GetMapping()
+    public int getMinimumCoins(@RequestParam BigDecimal billsAmount) throws Exception {
+        return service.findMiniumCoins(billsAmount);
     }
 }
